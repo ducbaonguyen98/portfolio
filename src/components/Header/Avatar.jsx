228dd0873b0a6f4d44dd80be1df8727687a86563
Bão Bão <ducbaonguyen101098@gmail.com>
2022-04-03 14:27:55 +0700

    up

--- src/components/Header/Avatar.jsx
+++ src/components/Header/Avatar.jsx
@@ -5,7 +5,7 @@ const Avatar = () => {
     
     <div class="border-4 md:border-0 w-[180px] h-[180px] md:w-[350px] md:h-[220px] border-[#F0F2F5] dark:border-dark-200 flex justify-center items-center rounded-full  md:rounded-none">
       <div class="md:shadow w-[160px] h-[160px] md:w-[330px] md:h-[200px] bg-gradient-to-t from-white to-[#F0F2F5] dark:from-dark dark:to-[#414141] rounded-full md:rounded-md flex justify-center items-center overflow-hidden">
-        <img class="rounded-full md:rounded-md self-end md:self-start" src="/static/media/avatar.d970e7251a0cc65c0d3f.png" alt="avatar" />
+        <img class="rounded-full md:rounded-md self-end md:self-start" src={require("../../images/avatar.png")} alt="avatar" />
       </div>
     </div>
   );
